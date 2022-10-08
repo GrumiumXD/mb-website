7136657717c3820db08212e76ffd8500cb31cc81
Gunnar Raßmann <g_rassmann@mail.de>
2022-10-08 19:50:20 +0200

    added language selection

--- frontend/src/layout/menu/Menu.tsx
+++ frontend/src/layout/menu/Menu.tsx
@@ -1,3 +1,5 @@
+import { MouseEvent } from 'react';
+
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
@@ -5,6 +7,8 @@ import StyledNavLink from '../../components/StyledNavLink';
 import { Tablet } from '../responsive';
 
 import banner from './banner.png';
+import flagDe from './flag-de.png';
+import flagEn from './flag-en.png';
 
 const StyledNav = styled.nav`
   display: flex;
@@ -14,6 +18,38 @@ const StyledNav = styled.nav`
   box-shadow: var(--box-shadow-m);
 
   grid-area: menu;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: baseline;
+`;
+
+const FlagButton = styled.button`
+  cursor: pointer;
+  border: none;
+  overflow: hidden;
+  border-radius: 0.25em;
+  box-shadow: var(--box-shadow-l);
+  outline: none;
+  padding: 0;
+  width: 40px;
+
+  &:active {
+    transform: scale(0.95, 0.95);
+    box-shadow: none;
+  }
+
+  &:focus-visible {
+    outline: 0.125em solid red;
+  }
+
+  & img {
+    display: block;
+    width: inherit;
+  }
 `;
 
 const MenuHeader = styled.h2`
@@ -32,15 +68,39 @@ const Banner = styled.img`
 type Props = {};
 
 const Menu = (props: Props) => {
-  const { t } = useTranslation('menu');
+  const { t, i18n } = useTranslation('menu');
+
+  // language switch event
+  const switchLanguage = (e: MouseEvent<HTMLButtonElement>) => {
+    if (e.currentTarget instanceof HTMLButtonElement) {
+      const attributes = e.currentTarget.attributes;
+      const label = attributes.getNamedItem('aria-label');
+
+      if (label != null) {
+        if (label.value === 'German') {
+          i18n.changeLanguage('de');
+        } else {
+          i18n.changeLanguage('en');
+        }
+      }
+    }
+  };
 
   return (
     <StyledNav>
-      <MenuHeader>{t('heading')}</MenuHeader>
+      <Header>
+        <FlagButton onClick={switchLanguage} aria-label="German">
+          <img src={flagDe} alt="german" aria-hidden="true" />
+        </FlagButton>
+        <MenuHeader>{t('heading')}</MenuHeader>
+        <FlagButton onClick={switchLanguage} aria-label="English">
+          <img src={flagEn} alt="english" aria-hidden="true" />
+        </FlagButton>
+      </Header>
       <StyledNavLink menu to={'/'} end>
         {t('about')}
       </StyledNavLink>
-      <StyledNavLink menu to={'gallery'} ex>
+      <StyledNavLink menu to={'gallery'}>
         {t('gallery')}
       </StyledNavLink>
       <StyledNavLink menu to={'songs'}>
@@ -49,7 +109,7 @@ const Menu = (props: Props) => {
       <StyledNavLink menu to={'interested'}>
         {t('interested')}
       </StyledNavLink>
-      <StyledNavLink menu to={'press'}>
+      <StyledNavLink men to={'press'}>
         {t('press')}
       </StyledNavLink>
       <StyledNavLink menu to={'contact'}>
